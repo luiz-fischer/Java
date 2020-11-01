175fa97f3c9f27b102ba475ca27a7d71bdc96807
luiz-fischer <luiz.fischer@alunos.sc.senac.br>
2020-11-01 00:13:23 -0300

    python

---
+++ Python/Intro.py
@@ -0,0 +1,48 @@
+print("Hello World!!") #This is a comment line
+if 5 > 2:
+    print("Five is greater than two!")
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+"""
+THIS IS A COMMENT MULTI LINE
+JUST WRITE
+"""
+# Variables
+x = 5 # this is a variable type int
+x = "Luiz" # this is a variable type str
+#Legal variable names
+myvar = "Luiz"
+my_var = "Luiz"
+_my_var = "Luiz"
+myVar = "Luiz"
+MYVAR = "Luiz"
+myvar2 = "Luiz"
+#Assign Value to Multiple Variables
+x, y, z = "Orange", "Banana", "Apple"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+#Output Variables
+x = "Luiz"
+print("My name is " + x)
+x = "My name "
+y = "Luiz"
+z = x + y
+print(z)
+x = 5
+y = 10
+print(x + y)
+#Global Variables
+x = "Luiz"
+
+def myFunc():
+    print("My name is " + x)
+
+myFunc()
+
